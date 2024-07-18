b50e15b0e1dab00cb15a90e67b40c14095f05d03
GANESHKUMAR <ganesh9ot@gmail.com>
2024-07-18 18:49:18 +0530

    # New problem string to key value is added.

---
+++ src/com/ganesh/StringToKeyValue.java
@@ -0,0 +1,13 @@
+package com.ganesh;
+
+// Given String s = "001xyz002xy003x"
+// output :  key   value
+//           001   xyz
+//           002   xy
+//           003   x
+public class StringToKeyValue {
+
+    public static void main(String[] args) {
+
+    }
+}
