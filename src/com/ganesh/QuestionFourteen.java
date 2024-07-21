ddf938950a955da4df11a41d84eb37d1c0b5aafe
GANESHKUMAR <ganesh9ot@gmail.com>
2024-07-21 16:46:15 +0530

    # Q.14 soln usingApacheStringUtils is added.

--- src/com/ganesh/QuestionFourteen.java
+++ src/com/ganesh/QuestionFourteen.java
@@ -1,4 +1,5 @@
 package com.ganesh;
+//import org.apache.commons.lang3.StringUtils;
 
 // Problem: WAP to Find Duplicate Characters in a String.
 // example String s = "123456"
@@ -32,4 +33,17 @@ public class QuestionFourteen {
     public static boolean usingStreamAPI(String input){
         return input.chars().allMatch(Character::isDigit);
     }
+
+    // 4. Using Apache Commons Lang Library
+    //Add the following dependency to your pom.xml file:
+//    <dependency>
+//    <groupId>org.apache.commons</groupId>
+//    <artifactId>commons-lang3</artifactId>
+//    <version>3.12.0</version>
+//    </dependency>
+
+//    public static boolean usingApacheStringUtils(String input) {
+//        return StringUtils.isNumeric(str);
+//    }
+
 }
