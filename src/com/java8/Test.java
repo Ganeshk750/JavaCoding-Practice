9eee39f3e4fe56c10b8ae1488ed2c22c8c14717d
GANESHKUMAR <ganesh9ot@gmail.com>
2025-01-06 21:02:55 +0530

    # Q. java 8 Q and code is added

---
+++ src/com/java8/Test.java
@@ -0,0 +1,33 @@
+package com.java8;
+
+import java.net.Inet4Address;
+import java.util.*;
+import java.util.stream.Collectors;
+
+/*
+*  WAP to group a list of string based on their lengths and count the number of string in each group.
+*  example ["dog", "cat","elephant","rabbit", "fox","owl","ant","zebra","giraffe"]
+*  output:-
+*  String of length 3: String eg:- dog cat fox owl....
+*  String of length 4: String eg:- zebra....
+*  String of length 6: String eg:- rabbit...
+* */
+public class Test {
+    public static void main(String[] args) {
+        // 1. Using java 8
+        List<String> stringList = Arrays.asList("dog", "cat", "elephant", "rabbit", "fox", "owl", "ant", "zebra", "giraffe");
+        Map<Integer, Long> collectedResult = stringList.stream().collect(Collectors.groupingBy(String::length, Collectors.counting()));
+        collectedResult.forEach((length, count) -> System.out.println("String of length "+ length + ": " + count));
+
+        // 2.
+        Map<Integer, Integer> groupByLength = new HashMap<>();
+        for(String str: stringList){
+            groupByLength.put(str.length(),groupByLength.getOrDefault(str.length(),0) + 1);
+        }
+
+        for(Map.Entry<Integer, Integer> entry : groupByLength.entrySet()){
+            System.out.println("String of length "+ entry.getKey() + ": "+ entry.getValue());
+        }
+
+    }
+}
